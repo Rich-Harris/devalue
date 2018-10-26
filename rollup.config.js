2da67f8340f66326994f74aca90620697b405303
Rich Harris <richard.a.harris@gmail.com>
2018-10-26 09:39:09 -0400

    -> v1.1.0

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import pkg from './package.json';
 export default {
 	input: 'src/index.ts',
 	output: [
-		{ file: pkg.main, format: 'umd', pkg.name },
+		{ file: pkg.main, format: 'umd', name: pkg.name },
 		{ file: pkg.module, format: 'es' }
 	],
 	plugins: [
