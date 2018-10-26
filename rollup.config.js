d848dfe39b0996d281829c52a256a988000c4d7b
Rich Harris <richard.a.harris@gmail.com>
2018-10-26 09:38:37 -0400

    -> v1.1.0

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,9 @@ import pkg from './package.json';
 export default {
 	input: 'src/index.ts',
 	output: [
-		{ file: pkg.main, format: 'umd' },
+		{ file: pkg.main, format: 'umd', pkg.name },
 		{ file: pkg.module, format: 'es' }
 	],
-	name: 'devalue',
 	plugins: [
 		typescript({
 			typescript: require('typescript')
