0259ccfccd6c416c52cbbe26a58cd63d1844675f
Matej Cerny <matej.cern@gmail.com>
2024-01-17 10:45:04 +0100

    feat: Add parseAsync & unflattenAsync

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 export { uneval } from './src/uneval.js';
-export { parse, unflatten } from './src/parse.js';
+export { parse, unflatten, parseAsync, unflattenAsync } from './src/parse.js';
 export { stringify } from './src/stringify.js';
